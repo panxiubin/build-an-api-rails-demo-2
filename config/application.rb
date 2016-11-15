39f98c5b0c774399ae0f9e439b9b71cc9ca4de15
xiubinpan@gmail.com <xiubinpan@gmail.com>
2016-11-15 14:16:28 +0800

    config application rate limit

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,6 @@
+# At the top of config/application.rb
+require 'rack/redis_throttle'
+
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
@@ -23,5 +26,6 @@ module BuildAnApiRailsDemo2
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.autoload_paths << Rails.root.join('app/policies')
+    config.middleware.use Rack::RedisThrottle::Daily, max: 3
   end
 end
