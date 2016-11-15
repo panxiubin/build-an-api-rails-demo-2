034d4e573074da1597cd4c8043a0d7aec8600dcf
xiubinpan@gmail.com <xiubinpan@gmail.com>
2016-11-15 10:47:42 +0800

    fix user controller syntax error

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::UsersController < Api::V1::BaseController
   def show
-    @user User.find(params[:id])
+    @user = User.find(params[:id])
 
     # 原文使用 Api::V1::UserSerializer
     # 我们现在使用 app/views/api/v1/users/show.json.jbuilder
