ee832ebfb9f6354ca6adec52b924b9ad5d9091a0
xiubinpan@gmail.com <xiubinpan@gmail.com>
2016-11-15 12:27:39 +0800

    use UserPolicy

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -11,6 +11,7 @@ class Api::V1::UsersController < Api::V1::BaseController
 
   def update
     @user = User.find(params[:id])
+    return api_error(status: 403) if !UserPolicy.new(current_user, @user).update?
     @user.update_attributes(update_params)
   end
 
@@ -19,5 +20,5 @@ class Api::V1::UsersController < Api::V1::BaseController
   def update_params
     params.require(:user).permit(:name)
   end
-  
+
 end
