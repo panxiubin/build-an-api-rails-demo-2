6e2f611922ffbfd7b763249bae7bd1d9a3da9930
xiubinpan@gmail.com <xiubinpan@gmail.com>
2016-11-15 11:43:54 +0800

    implement user name update method

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::UsersController < Api::V1::BaseController
+  before_action :authenticate_user!, only: [:update]
+
   def show
     @user = User.find(params[:id])
 
@@ -6,4 +8,16 @@ class Api::V1::UsersController < Api::V1::BaseController
     # 我们现在使用 app/views/api/v1/users/show.json.jbuilder
     # render(json: Api::V1::UserSerializer.new(user).to_json)
   end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update_attributes(update_params)
+  end
+
+  private
+
+  def update_params
+    params.require(:user).permit(:name)
+  end
+  
 end
