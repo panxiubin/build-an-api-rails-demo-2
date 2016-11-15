1bcab356dce5b5a59b86b3c248fb8d497d23762d
xiubinpan@gmail.com <xiubinpan@gmail.com>
2016-11-15 11:33:29 +0800

    test authenticate token

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -13,4 +13,8 @@ class Api::V1::BaseController < ApplicationController
   end
 
   attr_accessor :current_user
+
+  def api_error(opts = {})
+    render nothing: true, status: opts[:status]
+  end
 end
