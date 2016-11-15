9a8fdf1fe882abea09baaae087863935a84f638c
xiubinpan@gmail.com <xiubinpan@gmail.com>
2016-11-15 11:27:40 +0800

    config sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
       resources :users, only: [:index, :create, :show, :update, :destroy]
       # 原文有 microposts, 我们现在把它注释掉
       # resources :microposts, only: [:index, :create, :show, :update, :destroy]
+      resources :sessions, only: [:create]
     end
   end
   # The priority is based upon order of creation: first created -> highest priority.
